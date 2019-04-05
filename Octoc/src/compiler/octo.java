d360cbef0dcdb8143f807921639abeb5cad6d2e5
srihari unnikrishnan <srihari.unnikrishnan@gmail.com>
2019-04-05 16:41:16 +0530

    push binary

--- Octoc/src/compiler/octo.java
+++ Octoc/src/compiler/octo.java
@@ -1,10 +1,9 @@
-package compiler;import generator.Inbuilt;import java.util.ArrayList;public class octo{private static Inbuilt sys = new Inbuilt();public static String hello(String name,String king){return (name+king);}public static void main(String args[]){ArrayList<Object> a = sys.array("[53,\\hello,world\\,\\Srihari Unnikrishnan, Is the King\\,33]");
+package compiler;import generator.Inbuilt;import java.util.ArrayList;public class octo{private static Inbuilt sys = new Inbuilt();public static String hello(String name,String surname){return (name+surname);}public static void main(String args[]){ArrayList<Object> a = sys.array("[53,\\hello,world\\,\\Bananas\\,33]");
 if(5>3){
 int p = 31+35;
 sys.print (a.get(2));
-sys.print ("dang");
 if(3==1){
-sys.print ("my lang is an idiot. kinda");
+sys.print ("it works!");
 }
 }
 
@@ -12,5 +11,5 @@ for(int i:sys.prog(1,20)){
 sys.print(i);
 }
 
-sys.print(hello("Fortnite"," Cringey"));
+sys.print(hello("Srihari ","Unnikrishnan"));
 }}
